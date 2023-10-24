b6d2ed47c091d82f9df3a70edade0ba475a91214
yeo-chi <00www.black@gmail.com>
2023-10-24 12:44:24 +0900

    Controller 이름 변경

--- src/main/kotlin/com/example/todolist/user/controller/api/UserApiController.kt
+++ src/main/kotlin/com/example/todolist/user/controller/api/UserApiController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/users")
-class UserController(
+class UserApiController(
     private val userService: UserService,
 ) {
     @GetMapping("me")
