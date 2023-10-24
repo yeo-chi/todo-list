380986b1f1df8b04c242842454aafb3e583ae7dd
yeo-chi <00www.black@gmail.com>
2023-10-24 13:23:07 +0900

    jwt 적용하여 회원가입 로그인 기능 구현

--- src/main/kotlin/com/example/todolist/user/controller/api/UserApiController.kt
+++ src/main/kotlin/com/example/todolist/user/controller/api/UserApiController.kt
@@ -1,8 +1,7 @@
 package com.example.todolist.user.controller.api
 
-import com.example.todolist.user.controller.api.data.SignInUserRequest
-import com.example.todolist.user.controller.api.data.SignUpUserRequest
-import com.example.todolist.user.controller.api.data.UserResponse
+import com.example.todolist.configuration.jwt.TokenProvider
+import com.example.todolist.user.controller.api.data.*
 import com.example.todolist.user.persistent.entity.User
 import com.example.todolist.user.service.UserService
 import org.springframework.http.HttpStatus.*
@@ -12,6 +11,8 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("api/v1/users")
 class UserApiController(
     private val userService: UserService,
+
+    private val tokenProvider: TokenProvider,
 ) {
     @GetMapping("me")
     @ResponseStatus(OK)
@@ -21,20 +22,20 @@ class UserApiController(
 
     @PostMapping
     @ResponseStatus(CREATED)
-    fun signUp(@RequestBody signUpUserRequest: SignUpUserRequest): String {
-        userService.signUp(
-            user = User.of(signUpUserRequest = signUpUserRequest),
+    fun signUp(@RequestBody signUpUserRequest: SignUpUserRequest): SignUpUserResponse {
+        return SignUpUserResponse(
+            user = userService.signUp(user = User.of(signUpUserRequest = signUpUserRequest)),
+            tokenProvider = tokenProvider,
         )
-
-        TODO("JWT 를 이용하여 토큰을 발급해야함")
     }
 
     @PostMapping("signIn")
     @ResponseStatus(OK)
-    fun signIn(@RequestBody signInUserRequest: SignInUserRequest): String {
-        userService.signIn(signInUserRequest = signInUserRequest)
-
-        TODO("JWT 를 이용하여 토큰을 발급해야함")
+    fun signIn(@RequestBody signInUserRequest: SignInUserRequest): SignInUserResponse {
+        return SignInUserResponse(
+            user = userService.signIn(signInUserRequest = signInUserRequest),
+            tokenProvider = tokenProvider,
+        )
     }
 
     @DeleteMapping("me")
---
+++ src/main/kotlin/com/example/todolist/user/controller/api/data/SignUpUserResponse.kt
@@ -0,0 +1,24 @@
+package com.example.todolist.user.controller.api.data
+
+import com.example.todolist.configuration.jwt.TokenProvider
+import com.example.todolist.user.persistent.entity.User
+
+class SignUpUserResponse(
+    user: User,
+    tokenProvider: TokenProvider,
+) {
+    val id: Long
+
+    val userId: String
+
+    val nickName: String
+
+    val token: String
+
+    init {
+        id = user.id
+        userId = user.userId
+        nickName = user.nickName
+        token = tokenProvider.createJwtToken(userSpecification = id.toString())
+    }
+}
