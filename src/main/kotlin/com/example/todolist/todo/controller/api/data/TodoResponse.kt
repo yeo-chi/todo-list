397e9c81de11a7d9bb56129a327805e8d0d04970
yeo-chi <00www.black@gmail.com>
2023-10-24 13:45:50 +0900

    swagger 문제로인해 response 수정

--- src/main/kotlin/com/example/todolist/todo/controller/api/data/TodoResponse.kt
+++ src/main/kotlin/com/example/todolist/todo/controller/api/data/TodoResponse.kt
@@ -4,30 +4,28 @@ import com.example.todolist.todo.persistent.entity.Todo
 import com.example.todolist.todo.persistent.entity.data.TodoStatus
 import java.time.LocalDateTime
 
-class TodoResponse(
-    todo: Todo,
+data class TodoResponse(
+    val id: Long,
+
+    val title: String,
+
+    val memo: String?,
+
+    val startedAt: LocalDateTime,
+
+    val status: TodoStatus,
+
+    val createdAt: LocalDateTime,
+
+    val updatedAt: LocalDateTime?,
 ) {
-    val id: Long
-
-    val title: String
-
-    val memo: String?
-
-    val startedAt: LocalDateTime
-
-    val status: TodoStatus
-
-    val createdAt: LocalDateTime
-
-    val updatedAt: LocalDateTime?
-
-    init {
-        id = todo.id
-        title = todo.title
-        memo = todo.memo
-        startedAt = todo.startedAt
-        status = todo.status
-        createdAt = todo.createdAt
-        updatedAt = todo.updatedAt
-    }
+    constructor(todo: Todo) : this(
+        id = todo.id,
+        title = todo.title,
+        memo = todo.memo,
+        startedAt = todo.startedAt,
+        status = todo.status,
+        createdAt = todo.createdAt,
+        updatedAt = todo.updatedAt,
+    )
 }
